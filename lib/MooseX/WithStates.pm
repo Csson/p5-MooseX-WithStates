9890853d6354719f521611426aed86e12a0ab9a2
Erik Carlsson <info@code301.com>
2015-12-30 18:24:33 +0000

    Init

---
+++ lib/MooseX/WithStates.pm
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+package MooseX::WithStates;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use MooseX::WithStates;
+
+=head1 DESCRIPTION
+
+MooseX::WithStates is ...
+
+=head1 SEE ALSO
+
+=cut
